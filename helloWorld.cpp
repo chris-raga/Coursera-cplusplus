9280c37840449ebabd6ffcde0d14da49f403d8c0
Christian Alan Ragadio <christian.ragadio2@gmail.com>
2023-02-16 19:21:49 -0800

    goodbye world program

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+// header file used to to write input and outputs
+
 using namespace std;
+// standard namespace if you want more than one function with the same name 
+// grabbed from different libraries
 
 int main()
+// returns an integer, most fxns return an integer
 {
-    cout <<"goodbye world again";   
-    return 0;
+    cout <<"Goodbye World";  
+// cout =  character out << = insertion operator
+
+return 0;
+// defines success of program in this case - compiler adds it automatically 
+// tested and works
 }
