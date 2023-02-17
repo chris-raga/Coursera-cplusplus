f0ca3f6add93765ec3a59a271649d9c78f4641e7
Niti T <77049172+Sun-G-S6@users.noreply.github.com>
2023-02-16 20:06:19 -0800

    Update helloWorld.cpp
    here ya go buddy

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 // header file used to to write input and outputs
+#include <string>
 
 using namespace std;
 // standard namespace if you want more than one function with the same name 
@@ -8,10 +9,11 @@ using namespace std;
 int main()
 // returns an integer, most fxns return an integer
 {
+    string christian = "\t\t\t\tur U-G-L-Y"
     cout <<"Goodbye World";  
 // cout =  character out << = insertion operator
 
-return 0;
+    return 0;
 // defines success of program in this case - compiler adds it automatically 
 // tested and works
 }
