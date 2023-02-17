40196654af4653cfc85ba715bf7af28a2f09b13b
Christian Alan Ragadio <christian.ragadio2@gmail.com>
2023-02-16 20:32:16 -0800

    VERY IMPORTANT UPDATE

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 // standard namespace if you want more than one function with the same name 
 // grabbed from different libraries
-
-int main()
+    
 // returns an integer, most fxns return an integer
+int main()
 {
-    string greeting = "         YOU ARE VERYYYYYYYYYYYYYYYY UGLY ";
-    cout << greeting +"Goodbye World";  
-// cout =  character out << = insertion operator
+    string greeting = "\n YOU ARE VERYYYYYYYYYYYYYYYY UGLY";
+    string goodBye = "\n Goodbye Cruel World...";
+    cout << greeting + goodBye;
+    // cout =  character out << = insertion operator
 
-return 0;
-// defines success of program in this case - compiler adds it automatically 
-// tested and works
+    // defines success of program in this case - compiler adds it automatically 
+    // tested and works
+    return 0;
 }
